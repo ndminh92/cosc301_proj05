70a85ffc58cefac76c043310840db3d89b8b738b
ulysses-sl <mail@saklee.net>
2014-12-09 21:59:25 -0500

    Added basic bitmasking flags for cluster info

--- scandisk.c
+++ scandisk.c
@@ -15,8 +15,12 @@
 #include "dos.h"
 
 
-#define CLUSTER_IS_USED (1)
-#define CLUSTER_IS_POINTED (1 << 1)
+#define CLUSTER_ZEROMASK (0)
+#define CLUSTER_ALLMASK (255)
+#define CLUSTER_USED (1)
+#define CLUSTER_POINTED (1 << 1)
+#define CLUSTER_BAD (1 << 2)
+#define CLUSTER_MULTIPOINTED (1 << 3)
 
 
 
@@ -49,27 +53,23 @@ uint16_t print_dirent(struct direntry *dirent, int indent)
     extension[3] = ' ';
     memcpy(name, &(dirent->deName[0]), 8);
     memcpy(extension, dirent->deExtension, 3);
-    if (name[0] == SLOT_EMPTY)
-    {
+    if (name[0] == SLOT_EMPTY) {
 	return followclust;
     }
 
     /* skip over deleted entries */
-    if (((uint8_t)name[0]) == SLOT_DELETED)
-    {
+    if (((uint8_t)name[0]) == SLOT_DELETED) {
 	return followclust;
     }
 
-    if (((uint8_t)name[0]) == 0x2E)
-    {
+    if (((uint8_t)name[0]) == 0x2E) {
 	// dot entry ("." or "..")
 	// skip it
         return followclust;
     }
 
     /* names are space padded - remove the spaces */
-    for (i = 8; i > 0; i--) 
-    {
+    for (i = 8; i > 0; i--)  {
 	if (name[i] == ' ') 
 	    name[i] = '\0';
 	else 
@@ -77,38 +77,29 @@ uint16_t print_dirent(struct direntry *dirent, int indent)
     }
 
     /* remove the spaces from extensions */
-    for (i = 3; i > 0; i--) 
-    {
+    for (i = 3; i > 0; i--)  {
 	if (extension[i] == ' ') 
 	    extension[i] = '\0';
 	else 
 	    break;
     }
 
-    if ((dirent->deAttributes & ATTR_WIN95LFN) == ATTR_WIN95LFN)
-    {
+    if ((dirent->deAttributes & ATTR_WIN95LFN) == ATTR_WIN95LFN) {
 	// ignore any long file name extension entries
 	//
 	// printf("Win95 long-filename entry seq 0x%0x\n", dirent->deName[0]);
-    }
-    else if ((dirent->deAttributes & ATTR_VOLUME) != 0) 
-    {
+    } else if ((dirent->deAttributes & ATTR_VOLUME) != 0)  {
 	printf("Volume: %s\n", name);
-    } 
-    else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0) 
-    {
+    } else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0) {
         // don't deal with hidden directories; MacOS makes these
         // for trash directories and such; just ignore them.
-	if ((dirent->deAttributes & ATTR_HIDDEN) != ATTR_HIDDEN)
-        {
+	if ((dirent->deAttributes & ATTR_HIDDEN) != ATTR_HIDDEN) {
 	    print_indent(indent);
     	    printf("%s/ (directory)\n", name);
             file_cluster = getushort(dirent->deStartCluster);
             followclust = file_cluster;
         }
-    }
-    else 
-    {
+    } else {
         /*
          * a "regular" file entry
          * print attributes, size, starting cluster, etc.
@@ -185,13 +176,15 @@ int cluster_trace(uint16_t start_cluster, uint8_t *image_buf, struct bpb33 *bpb)
     return cluster_count;
 }
 
-void check_free_cluster(int[] *cluster_info, uint8_t *image_buf, struct bpb33 *bpb) {
+void check_free_cluster(uint8_t **cluster_info, uint8_t *image_buf, struct bpb33 *bpb) {
+    // Assumes cluster_info is clean and pristine
     uint16_t cluster = 0;
     for (int i = 2; i < bpb -> bpbSectors + 2; i++) {
         cluster = get_12masked_fat_entry(i, image_buf, bpb);
-        if (cluster == CLUST_FREE) {
-            *(cluster_info[i]) = *(cluster_info[i]) & 0x
+        if (cluster != CLUST_FREE) {
+            *(cluster_info[i]) &= CLUSTER_USED;
         }
+        // don't forget to check bad cluster!
     }
 
 }
@@ -211,6 +204,13 @@ int main(int argc, char** argv) {
     dos_ls(image_buf, bpb);
 
     int num_cluster = bpb -> bpbSectors;
+
+    // Array to keep track of cluster info
+    uint8_t cluster_info[num_cluster + 2];
+    for (int i = 0; i < num_cluster + 2; i++) {
+        cluster_info[i] = CLUSTER_ZEROMASK;
+    }
+
     int free_count = 1;
     for (int i = 2; i < num_cluster + 2; i++) {
         uint16_t cluster = get_fat_entry(i, image_buf, bpb);
